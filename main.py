0672c1d2be741bf64edd04a335cb6ab0e0610aa9
PixeLarm12 <lucasrdomingues-lrd@hotmail.com>
2024-05-03 19:36:07 -0300

    Cleaned code

--- main.py
+++ main.py
@@ -6,16 +6,17 @@ from sklearn.metrics import accuracy_score
 train_data = pd.read_csv("dataset/Training.csv")
 test_data = pd.read_csv("dataset/Testing.csv")
 
-# needs to be dropped last column created by an ',' at the end
+# needs to be dropped last column created by an ',' at the end - dataset formatting
 train_data = train_data.drop(train_data.columns[len(train_data.columns)-1], axis=1)
 
-# features => symptons
+# features => symptons (0 or 1)
 # target => prognosis
 
-# features (X) and target (Y)
-x_train = train_data.drop("prognosis", axis=1) # selects all columns but not prognosis
+# features (X) and target (Y) for TRAINING
+x_train = train_data.drop("prognosis", axis=1)
 y_train = train_data["prognosis"]
 
+# features (X) and target (Y) for TESTING SCORE
 x_test = test_data.drop("prognosis", axis=1)
 y_test = test_data["prognosis"]
 
@@ -25,10 +26,13 @@ model.fit(x_train, y_train)
 
 y_test_prediction = model.predict(x_test)
 
+# Testing model prediction score
 score = accuracy_score(y_test, y_test_prediction)
 
+# Some random values and suppose to return Fungal Infection as result
 x_for_testing = [[1,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]]
 
 model.fit(x_train.values, y_train)
-# prediction
 y_prediction = model.predict(x_for_testing)
+
+print(f"The current prognosis is '{y_prediction[0]}' with model accuracy score {round(score * 100, 4)}%")
