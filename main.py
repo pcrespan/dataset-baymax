8b033e4854ab871ebea9c5acf3995b03c366d523
PixeLarm12 <lucasrdomingues-lrd@hotmail.com>
2024-05-09 20:30:01 -0300

    Added new architeture

--- main.py
+++ main.py
@@ -1,5 +1,19 @@
 from src.DecisionTreeModel import useDecisionTree
+import random
+import numpy as np
 
-decisionTreeResult = useDecisionTree();
+# these variables needs to be changed for symptons collected from frontend and passed it to backend, then use them here
+num_features = 132
 
-print(decisionTreeResult);
+test_input = []
+
+# random list for predicting
+for i in range(num_features):
+    min_value = 0
+    max_value = 1
+    random_value = random.uniform(min_value, max_value)
+    test_input.append(random_value)
+
+dtResult = useDecisionTree(np.array(test_input).reshape(1, -1));
+
+print(f"The current prognosis is '{dtResult[0]}' with model accuracy score {dtResult[1]}%");
--- src/DecisionTreeModel.py
+++ src/DecisionTreeModel.py
@@ -1,46 +1,10 @@
-import pandas as pd
+from src.ReadDatasets import getTestingData, getTrainingData
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.metrics import accuracy_score
-import random
-import numpy as np
 
-def useDecisionTree():
-    # reading data from drive
-    train_data = pd.read_csv("dataset/Training.csv")
-    test_data = pd.read_csv("dataset/Testing.csv")
-
-    # finding missing values TEST DATA
-    missing_values = test_data.isnull()
-
-    # counting missing values TEST DATA
-    missing_count = missing_values.sum()
-
-    print("Missing values per column:")
-    print(missing_count)
-
-    # finding missing values TRAIN DATA
-    missing_values = train_data.isnull()
-
-    # counting missing values TRAIN DATA
-    missing_count = missing_values.sum()
-
-    print("Missing values per column:")
-    print(missing_count)
-
-    print(train_data['Unnamed: 133'])
-
-    train_data.drop(columns=['Unnamed: 133'], inplace=True)
-
-    # features => symptons (0 or 1)
-    # target => prognosis
-
-    # features (X) and target (Y) for TRAINING
-    x_train = train_data.drop(columns=["prognosis"])
-    y_train = train_data["prognosis"]
-
-    # features (X) and target (Y) for TESTING SCORE
-    x_test = test_data.drop(columns=["prognosis"])
-    y_test = test_data["prognosis"]
+def useDecisionTree(test_values):
+    x_train, y_train = getTrainingData();    
+    x_test, y_test = getTestingData();    
 
     # training model
     model = DecisionTreeClassifier(random_state=42)
@@ -52,24 +16,7 @@ def useDecisionTree():
 
     print("Acc score: {:.2f}%".format(score * 100))
 
-    # num features
-    num_features = x_train.shape[1]
-
-    print("Features count:", num_features)
-
-    test_input = []
-
-    # random list for predicting
-    for i in range(num_features):
-        min_value = 0
-        max_value = 1
-        random_value = random.uniform(min_value, max_value)
-        test_input.append(random_value)
-
-    # convert to numpy array
-    test_input_array = np.array(test_input).reshape(1, -1)
-
     # model predict
-    prediction = model.predict(test_input_array)
+    prediction = model.predict(test_values)
 
-    return f"The current prognosis is '{prediction[0]}' with model accuracy score {round(score * 100, 4)}%"
+    return [prediction[0], round(score * 100, 2)]
