ceb92adcd382d000264b696458e288748b332626
PixeLarm12 <lucasrdomingues-lrd@hotmail.com>
2024-05-23 20:21:17 -0300

    Fixed SVC model 100% acc

--- src/Predictions/SVCModel.py
+++ src/Predictions/SVCModel.py
@@ -1,20 +1,25 @@
 from src.Utils.ReadDatasets import getTestingData, getTrainingData
 from sklearn.svm import SVC
 from sklearn.metrics import accuracy_score
+from sklearn.preprocessing import StandardScaler
 
 def useSVC(test_values):
     x_train, y_train = getTrainingData();    
-    x_test, y_test = getTestingData();    
+    x_test, y_test = getTestingData();  
 
-    # training model
-    model = SVC()
+    scaler = StandardScaler()
+    x_train = scaler.fit_transform(x_train)
+    x_test = scaler.transform(x_test)
+
+    model = SVC(kernel='linear', C=1.0)
     model.fit(x_train, y_train)
 
     y_test_prediction = model.predict(x_test)
 
     score = accuracy_score(y_test, y_test_prediction)
 
-    # model predict
+    test_values = scaler.transform(test_values)
+    
     prediction = model.predict(test_values)
 
     return [prediction[0], round(score * 100, 2)]
